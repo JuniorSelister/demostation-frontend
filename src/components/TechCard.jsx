4df52b05372422caed5896db34580713d97bb9f7
JuniorSelister <junior.selister@gmail.com>
2022-10-25 16:13:20 -0300

    Layout changes

--- src/components/TechCard.jsx
+++ src/components/TechCard.jsx
@@ -4,12 +4,10 @@ import data from "./data";
 
 export default () => {
   return (
-    <div className="card">
+    <div className="card-layout">
       {data.map((datas) => (
-        <div key={datas.id}>
-          <div className="card-image">
-            <img src={datas.img} alt="Icons" />
-          </div>
+        <div key={datas.id} className="card">
+          <img src={datas.img} alt="Icons" className="card-image"/>
           <h2 className="card-title">{datas.title}</h2>
           <p className="card-info">{datas.text}</p>
         </div>
